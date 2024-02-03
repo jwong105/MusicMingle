d26147c647773a44d68fb3c1721aa6a5e3ad8749
Jwong105 <jennifer.wong105@gmail.com>
2024-02-03 15:15:27 -0800

    Switch to mongoose. Debug record.js to add, update, delete and get items from the database

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,31 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const config = require('./config.env'); // adjust the path based on your file structure
+
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-   });
-  console.log(`Server is running on port: ${port}`);
+const port = config.PORT || 3001;
+
+app.use(express.json());  // Move this line here
+
+app.use(require("./record"));
+
+const uri = config.URI;
+
+console.log('Connection string:', uri);
+
+// Connect to MongoDB using the Atlas URI
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Successfully connected to MongoDB.');
+
+  // Use the recordRoutes in your application
+  // app.use('/api', recordRoutes);
+
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
 });
