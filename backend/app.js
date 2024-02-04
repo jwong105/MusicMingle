045cfe4b935f29cb03b0389a48b91fb367d4103e
Jwong105 <jennifer.wong105@gmail.com>
2024-02-03 19:19:15 -0800

    Update database schema

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,26 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const config = require('./config.env'); // adjust the path based on your file structure
+const config = require('./config.env');
 
 const app = express();
 const port = config.PORT || 3001;
 
-app.use(express.json());  // Move this line here
+app.use(express.json());
 
-app.use(require("./record"));
+app.use(require("./profile"));
 
 const uri = config.URI;
-
-console.log('Connection string:', uri);
 
 // Connect to MongoDB using the Atlas URI
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Create reference to database connection
 const db = mongoose.connection;
+
+// Connect the database
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Successfully connected to MongoDB.');
-
-  // Use the recordRoutes in your application
-  // app.use('/api', recordRoutes);
 
   // Start the server
   app.listen(port, () => {
